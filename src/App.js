c8ff56b58e87de8ee3322523f40160c7a72fa847
= <=>
2022-11-07 15:08:01 -0500

    header & button done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Hello from './Components/Hello';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (1===1) {
+    return(
+      <div className='App container-fluid bg-light'>
+          <div className='px-3 py-4'>
+            <button id='btn-div' className='px-3'>click</button>
+          </div>
+          <Hello />
+      </div>
+    );
+  }
+  if (1===2) {
+    return(
+      <div className='App container-fluid bg-dark'>
+          <div className='px-3 py-4'>
+            <button id='btn-div' className='bg-dark text-white border-white px-3'>click</button>
+          </div>
+          <Hello />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Hello.js
@@ -0,0 +1,24 @@
+import { React, useState } from 'react';
+
+export default function Hello() {
+
+    /*const [ name, setName ] = useState('');
+    const [ quantity, setQuantity ] = useState('');*/
+
+    if (1===1) {
+        return(
+            <div className="container-fluid p-5">
+              <h1 className='text-center'>hello</h1>
+            </div>
+        );
+      }
+      if (1===2) {
+        return(
+            <div className="container-fluid p-5">
+              <h1 className='text-center text-white'>hello</h1>
+            </div>
+        );
+      }
+}
+        
+        
